e23bb9dc0b3d4158d27336a85abd9156921e1037
dawn-cmd <hjing7@wisc.edu>
2023-03-05 11:07:54 -0600

    q

--- RustLearning/variables/src/main.rs
+++ RustLearning/variables/src/main.rs
@@ -1,4 +1,11 @@
 fn main() {
-    let a = [0; 10];
-    println!("{}", a[0]);
+    let x = 10;
+    println!("{}", fibo(x));
+}
+fn fibo(x: i32) -> i32{
+    if x == 0 || x == 1 {
+        x
+    } else {
+        fibo(x - 1) + fibo(x - 2)
+    }
 }
