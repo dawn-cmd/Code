892403c891958655fda6c3893dcdf889f4401468
dawn-cmd <hjing7@wisc.edu>
2023-03-09 11:32:04 -0600

    rust_struct

---
+++ RustLearning/struct_learn/src/main.rs
@@ -0,0 +1,17 @@
+#[derive(Debug)]
+struct Rect {
+    width: u32,
+    height: u32,
+}
+impl Rect {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+fn main() {
+    let rect = Rect {
+        width: 30,
+        height: 50,
+    };
+    println!("The area of rectangle is {}.", rect.area());
+}
